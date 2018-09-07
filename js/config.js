7b711f90b85e5ce9b906070ce3a7caecdf623058
Alejandro Colomina <alejandro.colomina@flywire.com>
2018-09-07 15:50:04 +0200

    [SREFTP-939] Prepared development environment

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var AWS_OIDC_PROVIDER_URL = 'flywire.oktapreview.com';
+var AWS_ROLE_ARN = 'arn:aws:iam::306077209789:role/flywire-okta-oidc-aws-test-role';
+var AWS_REGION = 'us-east-1';
+var AWS_S3_BUCKET_NAME = 'recon_bankfiles-staging-flywire';
+var OKTA_ORG_URL = 'https://flywire.oktapreview.com';
+var OKTA_CLIENT_ID = '0oag07d8xpcriufjQ0h7';
